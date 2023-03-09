eaa45f8feb9512359f37c3e135d14ab5b74eb2de
“saka-git <sakamoto.tatsuya1997@gmail.com>
2023-03-09 20:12:27 +0900

    feat: カテゴリ、メニューテーブル追加

---
+++ database/migrations/2023_03_09_192318_create_training_menus_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('training_menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('training_category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('training_menus');
+    }
+};
