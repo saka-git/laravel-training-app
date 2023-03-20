c8bfcb2069f80030d115be71bdff4801191749a6
“saka-git <sakamoto.tatsuya1997@gmail.com>
2023-03-20 14:33:50 +0900

    feat: トレーニングリザルト追加機能（仮）

--- resources/views/modals/add_training_result.blade.php
+++ resources/views/modals/add_training_result.blade.php
@@ -15,20 +15,21 @@
         @csrf
         <div class="modal-body">
           <div id="training-result-form">
+            <input type="date" name="date">
             <label>トレーニングカテゴリ</label>
-            <select class="form-select mb-3" name="training_category_id">
+            <select class="form-select mb-3" name="training_menu_id">
               @foreach ($training_menus as $training_menu)
               <option value="{{ $training_menu->id }}">{{ $training_menu->name }}</option>
               @endforeach
             </select>
             <label>1set目</label>
             <div clqss="row w-100">
-              <div class="input-group mb-3 w-30">
+              <div class="input-group mb-3">
                 <input type="number" class="form-control" placeholder="100" aria-describedby="weight" name="weight">
                 <span class="input-group-text" id="weight">kg</span>
               </div>
               <div>×</div>
-              <div class="input-group mb-3 w-30">
+              <div class="input-group mb-3">
                 <input type="number" class="form-control" placeholder="10" aria-describedby="rep" name="rep">
                 <span class="input-group-text" id="rep">回</span>
               </div>
